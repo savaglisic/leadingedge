f773235f0da7594e5a5098ab2c94ded0dfaef58a
Sava Glisic <104223515+savaglisic@users.noreply.github.com>
2024-02-25 09:18:55 -0500

    Created structure, service and components

---
+++ src/app/components/image-preview/image-preview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-preview',
+  templateUrl: './image-preview.component.html',
+  styleUrls: ['./image-preview.component.css']
+})
+export class ImagePreviewComponent {
+
+}
